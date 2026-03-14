2223973c17f4109e67c11d7d406a24389887dda0
redun <redunsasha2017@gmail.com>
2026-03-14 21:16:40 +0700

    create models with relationship

---
+++ src/models/profile.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models import Base
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Profile(Base):
+    name: Mapped[str] = mapped_column(String(50), nullable=True)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    photo: Mapped[str] = mapped_column(nullable=True)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        unique=True,
+    )
+    user: Mapped["User"] = relationship(
+        back_populates="profile",
+        foreign_keys=[user_id],
+    )
---
+++ src/models/user.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models import Base
+
+if TYPE_CHECKING:
+    from .profile import Profile
+
+
+class User(Base):
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[bytes] # 8 - 16 символов ?
+    reset_password: Mapped[bytes] = mapped_column(nullable=True)
+    confirm_code: Mapped[str] = mapped_column(nullable=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+
+    profile: Mapped["Profile"] = relationship(back_populates="user")
