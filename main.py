64a1d346e8ff7add4142b69521f3961746628243
redun <redunsasha2017@gmail.com>
2026-03-14 21:12:26 +0700

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from src.api.views import router as api_router
+from src.config import settings
+from src.database.db import db_helper
+from src.models import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # async with db_helper.engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
+    #
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=settings.port, reload=True)
