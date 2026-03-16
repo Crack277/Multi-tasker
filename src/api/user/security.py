ccc43626fc81cbf83dff58f8a48496c7c7ed15eb
redun <redunsasha2017@gmail.com>
2026-03-16 20:43:42 +0700

    fix and refactoring code

--- src/api/user/security.py
+++ src/api/user/security.py
@@ -1,93 +1,126 @@
-import json
-import secrets
 from datetime import datetime, timedelta
 
-import bcrypt
 import jwt
-from fastapi import HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from redis.auth.token import JWToken
 from src.config import settings
-from src.redis import redis_client
+from src.database.db import db_helper
+from src.models import User
 
-security = HTTPBasic()
+security = HTTPBearer()
 
-
-def get_hashed_password(password: str) -> bytes:
-    password_bytes = password.encode("utf-8")
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    return hashed_password
+pwd_context = CryptContext(schemes=["bcrypt", "pbkdf2_sha256"], deprecated="auto")
 
 
-def create_access_token(data: dict) -> str:
+def get_hashed_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hash_password):
+    return pwd_context.verify(plain_password, hash_password)
+
+
+def create_access_token(data: dict) -> dict:
     encode_data = data.copy()
     expire = datetime.now() + timedelta(minutes=settings.access_token.expire_time)
     encode_data.update({"exp": expire, "type": "access"})
-    token = jwt.encode(encode_data, settings.access_token.secret_key, settings.access_token.ALGORITHM)
-    return token
-
-
-def create_refresh_token(data: dict) -> str:
-
-    refresh_token = secrets.token_urlsafe(32)
-
-    # Сохранение токена в Redis с временем жизни
-    redis_client.setex(
-        name=f"refresh_token:{refresh_token}",
-        time=settings.access_token.refresh_expire_time * 86_400,  # в секундах
-        value={
-            "user_email": data["email"],
-            "created_at": datetime.now(),
-        }
+    token = jwt.encode(
+        encode_data, settings.access_token.secret_key, settings.access_token.ALGORITHM
     )
-
-    return refresh_token
-
-
-def create_token_pair(data: dict) -> dict:
-
-    access_token = create_access_token(data)
-    refresh_token = create_refresh_token(data)
-
     return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
+        "access_token": token,
+        "type": "bearer",
     }
 
 
-def verify_access_token(access_token: str):
+async def get_current_user(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    credential: HTTPAuthorizationCredentials = Depends(security),
+):
 
-    exc = HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Incorrect access token!",
+    if credential is None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authenticated",
+        )
+
+    token = credential.credentials
+
+    payload = jwt.decode(
+        token, settings.access_token.secret_key, settings.access_token.ALGORITHM
     )
 
-    try:
-        payload = jwt.decode(
-            jwt=access_token,
-            key=settings.access_token.secret_key,
-            algorithms=[settings.access_token.ALGORITHM],
-        )
+    email = payload.get("email")
+    stmt = select(User).where(User.email == email)
+    user = await session.scalar(stmt)
+    payload.update({"user_id": user.id})
 
-        if payload.get("type") != "access":
-            raise exc
-
-        return payload
-
-    except JWToken:
-        raise exc
+    return payload
 
 
-def verify_refresh_token(refresh_token: str):
-
-    data = redis_client.get(f"refresh_token:{refresh_token}")
-
-    if data is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Incorrect refresh token!"
-        )
-
-    return data
+# def create_refresh_token(data: dict) -> str:
+#
+#     refresh_token = secrets.token_urlsafe(32)
+#
+#     # Сохранение токена в Redis с временем жизни
+#     redis_client.setex(
+#         name=f"refresh_token:{refresh_token}",
+#         time=settings.access_token.refresh_expire_time * 86_400,  # в секундах
+#         value=json.dumps({
+#             "user_email": data["email"],
+#             "created_at": datetime.now().isoformat(),
+#         })
+#     )
+#
+#     return refresh_token
+#
+#
+# def create_token_pair(data: dict) -> dict:
+#
+#     access_token = create_access_token(data)
+#     refresh_token = create_refresh_token(data)
+#
+#     return {
+#         "access_token": access_token,
+#         "refresh_token": refresh_token,
+#     }
+#
+#
+# def verify_access_token(access_token: str):
+#
+#     exc = HTTPException(
+#         status_code=status.HTTP_404_NOT_FOUND,
+#         detail="Incorrect access token!",
+#     )
+#
+#     try:
+#         payload = jwt.decode(
+#             jwt=access_token,
+#             key=settings.access_token.secret_key,
+#             algorithms=[settings.access_token.ALGORITHM],
+#         )
+#
+#         if payload.get("type") != "access":
+#             raise exc
+#
+#         return payload
+#
+#     except JWToken:
+#         raise exc
+#
+#
+# def verify_refresh_token(refresh_token: str):
+#
+#     data = redis_client.get(f"refresh_token:{refresh_token}")
+#
+#     if data is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail="Incorrect refresh token!"
+#         )
+#
+#     return data
