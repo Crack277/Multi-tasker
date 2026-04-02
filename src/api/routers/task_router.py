9039136393431da797e802cffde3519750a397b5
redun <redunsasha2017@gmail.com>
2026-04-02 21:50:59 +0700

    task views

--- src/api/routers/task_router.py
+++ src/api/routers/task_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.schemas.task_schemas import TaskCreate
@@ -11,7 +11,44 @@ from src.models import User
 router = APIRouter(prefix=settings.api.v1.tasks, tags=["TASKS"])
 
 
-@router.post("/create/{task_id}")
+@router.get("/all")
+async def get_tasks(
+    page: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.get_tasks(page=page)
+
+
+@router.get("/{task_id}/")
+async def get_task_by_id(
+    task_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.get_task_by_id(task_id=task_id)
+
+
+@router.get("/{user_id}/completed")
+async def get_user_completed_tasks(
+    user_id: int,
+    page: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.get_user_completed_tasks(user_id=user_id, page=page)
+
+
+@router.get("/{user_id}/info")
+async def get_user_tasks_info(
+    user_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.get_user_tasks_info(user_id=user_id)
+
+
+@router.post("/{task_id}/")
 async def create_task(
     task_create: TaskCreate,
     current_user: User = Depends(security.get_current_user),
@@ -19,3 +56,34 @@ async def create_task(
 ):
     service = TaskService(session)
     return await service.create_task(task_create=task_create, current_user=current_user)
+
+
+@router.post("/{task_id}/complete")
+async def complete_task(
+    task_id: int,
+    current_user: User = Depends(security.get_current_user),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.complete_task(task_id=task_id, current_user=current_user)
+
+
+@router.get("/")
+async def get_user_tasks_by_priority(
+    page: int,
+    very_urgent: bool = Query(False),
+    urgent: bool = Query(False),
+    can_wait: bool = Query(False),
+    not_urgent: bool = Query(False),
+    current_user: User = Depends(security.get_current_user),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    service = TaskService(session)
+    return await service.get_user_tasks_by_priority(
+        page=page,
+        very_urgent=very_urgent,
+        urgent=urgent,
+        can_wait=can_wait,
+        not_urgent=not_urgent,
+        current_user=current_user,
+    )
--- src/api/services/task_service.py
+++ src/api/services/task_service.py
@@ -1,8 +1,12 @@
+from typing import List
+
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.repositories.user_repository import UserRepository
-from src.api.schemas.task_schemas import TaskCreate
+from src.api.schemas.task_schemas import TaskCreate, UserTasksInfo
 from src.models import User
+from src.models.task import Task, TaskPriority, TaskStatus
 
 
 class TaskService:
@@ -10,7 +14,82 @@ class TaskService:
         self.repository = UserRepository(session)
         self.session = session
 
-    async def create_task(self, task_create: TaskCreate, current_user: User):
+    async def get_tasks(self, page: int) -> List[Task]:
+        return await self.repository.get_tasks(page=page)
+
+    async def get_task_by_id(self, task_id: int) -> Task:
+        return await self.repository.get_task_by_id(task_id=task_id)
+
+    async def get_user_completed_tasks(self, user_id: int, page: int) -> List[Task]:
+        user = await self.repository.get_user_by_id(user_id=user_id)
+        tasks = await self.repository.get_user_tasks(user_id=user.id, page=page)
+
+        completed_tasks = [
+            task for task in tasks if task.status == TaskStatus.COMPLETED
+        ]
+        return completed_tasks
+
+    async def get_user_tasks_by_priority(
+        self,
+        page: int,
+        very_urgent: bool,
+        urgent: bool,
+        can_wait: bool,
+        not_urgent: bool,
+        current_user: User,
+    ) -> List[Task]:
+        selected_count = sum([very_urgent, urgent, can_wait, not_urgent])
+        tasks = []
+
+        if selected_count == 1:
+            if very_urgent:
+                tasks = await self.repository.get_user_priority_tasks(
+                    user_id=current_user.id,
+                    priority=TaskPriority.VERY_URGENT,
+                    page=page,
+                )
+            if urgent:
+                tasks = await self.repository.get_user_priority_tasks(
+                    user_id=current_user.id,
+                    priority=TaskPriority.URGENT,
+                    page=page,
+                )
+            if can_wait:
+                tasks = await self.repository.get_user_priority_tasks(
+                    user_id=current_user.id,
+                    priority=TaskPriority.CAN_WAIT,
+                    page=page,
+                )
+            if not_urgent:
+                tasks = await self.repository.get_user_priority_tasks(
+                    user_id=current_user.id,
+                    priority=TaskPriority.NOT_URGENT,
+                    page=page,
+                )
+
+            return tasks
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="There should be one priority!",
+        )
+
+    async def get_user_tasks_info(self, user_id: int) -> UserTasksInfo:
+        user = await self.repository.get_user_by_id(user_id=user_id)
+        return await self.repository.get_user_tasks_info(user_id=user.id)
+
+    async def create_task(self, task_create: TaskCreate, current_user: User) -> Task:
         return await self.repository.create_task(
             task_create=task_create, current_user=current_user
+        )
+
+    async def complete_task(self, task_id: int, current_user: User) -> Task:
+        task = await self.repository.get_task_by_id(task_id=task_id)
+
+        if task.assignee_id == current_user.id or task.author_id == current_user.id:
+            return await self.repository.complete_task(task_id=task_id)
+
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="This is not your task!",
         )
