c63714a43be9f84448e69fc0db0d21c92fb21c2a
redun <redunsasha2017@gmail.com>
2026-04-02 21:50:37 +0700

    create priority and status for Task

--- src/models/task.py
+++ src/models/task.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+from enum import Enum
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, ForeignKey, String, Text
+from sqlalchemy import DateTime
+from sqlalchemy import Enum as SQLAlchemy_Enum
+from sqlalchemy import ForeignKey, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models import Base
@@ -12,11 +15,32 @@ if TYPE_CHECKING:
     from .user import User
 
 
+class TaskPriority(str, Enum):
+    VERY_URGENT = "very_urgent"  # очень срочно (красный)
+    URGENT = "urgent"  # срочно (оранжевый)
+    CAN_WAIT = "can_wait"  # может подождать (желтый)
+    NOT_URGENT = "not_urgent"  # не срочно (зеленый)
+
+
+class TaskStatus(str, Enum):
+    IN_PROGRESS = "in_progress"  # в работе
+    COMPLETED = "completed"  # выполнена
+
+
 class Task(Base):
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
-    priority: Mapped[str] = mapped_column(String(100), nullable=False)  # Mapped[type?]
-    status: Mapped[str] = mapped_column(String(100), nullable=False)  # Mapped[type?]
+
+    priority: Mapped[TaskPriority] = mapped_column(
+        SQLAlchemy_Enum(TaskPriority),
+        default=TaskPriority.NOT_URGENT,
+        nullable=False,
+    )
+    status: Mapped[TaskStatus] = mapped_column(
+        SQLAlchemy_Enum(TaskStatus),
+        default=TaskStatus.IN_PROGRESS,
+        nullable=False,
+    )
 
     deadline: Mapped[datetime | None] = mapped_column(DateTime, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
